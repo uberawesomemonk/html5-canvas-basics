2dceb888e0e059c7d49417e643e3db3ed8b820ac
unknown <csengineer13@gmail.com>
2015-10-05 15:20:28 -0400

    Pulling changes made in 'gh-pages' branch

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # html5-canvas-basics
 Shared repository to play with HTML5 Canvas and learn the basics about it
+
+Served at: [http://djb-code.github.io/html5-canvas-basics/](http://djb-code.github.io/html5-canvas-basics/)
--- src/app.js
+++ src/app.js
@@ -1,41 +1,46 @@
-// Static Vars
-var CANVAS_WIDTH = 480;
-var CANVAS_HEIGHT = 320;
-var FPS = 30;
+// Don't start until domReady
+$(function(){
 
-// Create and insert DOM
-var canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
-                      "' height='" + CANVAS_HEIGHT + "'></canvas>");
-var canvas = canvasElement.get(0).getContext("2d");
-canvasElement.appendTo('body');
+	// Static Vars
+	var CANVAS_WIDTH = 480;
+	var CANVAS_HEIGHT = 320;
+	var FPS = 30;
 
-
-// Game loop
-// 
-// Run this code every X seconds
-setInterval(function() {
-  update();
-  draw();
-}, 1000/FPS);
+	// Create and insert DOM
+	var canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
+	                      "' height='" + CANVAS_HEIGHT + "'></canvas>");
+	var canvas = canvasElement.get(0).getContext("2d");
+	canvasElement.appendTo('body');
 
 
-// Init vars
-var textX = 50;
-var textY = 50;
+	// Game loop
+	// 
+	// Run this code every X seconds
+	setInterval(function() {
+	  update();
+	  draw();
+	}, 1000/FPS);
 
 
-// Update method
-function update() {
-	textX += 1;
-	textY += 1;
-}
+	// Init vars
+	var textX = 50;
+	var textY = 50;
 
-// Draw the update
-function draw() {
-	// We have to clear our screen on each draw
-	canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-	// Redraw
-	canvas.fillStyle = "#000";
-	canvas.fillText("Sup Bro!", textX, textY);
-}
+	// Update method
+	function update() {
+		textX += 1;
+		textY += 1;
+	}
+
+	// Draw the update
+	function draw() {
+		// We have to clear our screen on each draw
+		canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+		// Redraw
+		canvas.fillStyle = "#000";
+		canvas.fillText("Sup Bro!", textX, textY);
+	}
+
+});
